72d9e678b9c8fa8a7a0c8f2711064e513999e7e4
NISH1001 <nish_play1001@yahoo.com>
2015-01-19 23:26:03 +0545

    Group Schedule Input Added

--- xmlwriter.py
+++ xmlwriter.py
@@ -4,34 +4,74 @@ import xml.etree.ElementTree as ElemTree
 
 days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
-def GenerateTable(id):
+class InputError(Exception):
+	def __init__(self, arg):
+		self.args = arg
+	def display(self):
+		print(''.join(self.args))
+
+def GenerateGroupTable(schedule):
 	Table = Element("Table")
 	Group = Element("Group")
-	Group.attrib["id"] = str(id)
+	Group.attrib["id"] = schedule["id"]
 	Table.append(Group)
 	Day = [ Element(day) for day in days]
 
 	for i in range(len(Day)):
-		time = Element("Time")
-		time.attrib["start"] = "12:00"
-		time.attrib["end"] = "12:00"
-		Day[i].append(time)
+		scheduletime = schedule[days[i]]
+		for sch in scheduletime:
+			time = Element("Time") 
+			time.attrib["end"] = sch[1]
+			time.attrib["start"] = sch[0]
+			Day[i].append(time)
 	Group.extend(Day)
 	return Table
 
+"""Returns a dict as:
+	['id'] = ""
+	['Sunday'] = [[1,2], [3,4]]
+	['Monday'] = [[5,6]]
+"""
+
+def InputRoutine():
+	routine = {}
+	try:
+		id = input("Which Group ? : ")
+		if int(id)<1 or int(id) >7:
+			raise InputError("Invalid Group ID")
+		else:
+			routine['id'] = id
+	except InputError as e:
+		e.display()
+		return {}
+	else: 
+		print("yay")
+
+	for day in days:
+		routine[day] = []
+		fin = False
+		while not fin:
+			print(day, ":")
+			start = input("Enter start time : ")
+			end = input("Etner end time : ")
+			time = [start,end]
+			routine[day].append(time)
+			select = input("Add another?(y/n) : ")
+			if select == 'n':
+				fin = True
+	return routine
 
 
 def main():
+	schedule = InputRoutine()
 	root = Element('Routine')
 	tree = ElementTree(root)
 
-	#id = int(input("Enter group : "))
-
-	Table = GenerateTable(1)
+	if schedule:
+		Table = GenerateGroupTable(schedule)
+		
 	root.append(Table)
-
 	xml = prettify(root)
-	#print(prettify(root))
 	with open("test.xml","w") as routine:
 		for line in xml:
 			routine.write(line)
