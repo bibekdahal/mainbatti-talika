e3f592ac7e398567539618e428dda14d82a2df58
NISH1001 <nish_play1001@yahoo.com>
2015-01-19 21:10:20 +0545

    test writer

---
+++ xmlwriter.py
@@ -0,0 +1,41 @@
+from xml.etree.ElementTree import Element, SubElement, Comment, ElementTree
+from ElementTree_Pretty import prettify
+import xml.etree.ElementTree as ElemTree
+
+days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+def GenerateTable(id):
+	Table = Element("Table")
+	Group = Element("Group")
+	Group.attrib["id"] = str(id)
+	Table.append(Group)
+	Day = [ Element(day) for day in days]
+
+	for i in range(len(Day)):
+		time = Element("Time")
+		time.attrib["start"] = "12:00"
+		time.attrib["end"] = "12:00"
+		Day[i].append(time)
+	Group.extend(Day)
+	return Table
+
+
+
+def main():
+	root = Element('Routine')
+	tree = ElementTree(root)
+
+	id = int(input("Enter group : "))
+
+	Table = GenerateTable(id)
+	root.append(Table)
+
+	x = prettify(root)
+	print(prettify(root))
+
+	with open("test.xml","w") as routine:
+		for line in x:
+			routine.write(line)
+
+if __name__=="__main__":
+	main()
