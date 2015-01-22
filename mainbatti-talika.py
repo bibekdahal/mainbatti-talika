1e8157baa182bc076ba7c3ee3e2ffa31b017c301
NISH1001 <nish_play1001@yahoo.com>
2015-01-22 16:49:59 +0545

    Command line parser added

--- mainbatti-talika.py
+++ mainbatti-talika.py
@@ -1,9 +1,70 @@
 #!/usr/bin/env python3
 
+import sys
 import RoutineReader
+import re
+
+class ArgumentError(Exception):
+	def __init__(self,arg):
+		self.arg = arg
+	def display(self):
+		print(''.join(self.args))
+
+def IsGroup(input):
+	group_re = re.compile(r'^g[1-7]$')
+	return bool(group_re.match(input))
+
+def IsTime(input):
+	time_re = re.compile(r'^(-twelve)|(--12)$')
+	return bool(time_re.match(input))
+
+def IsNepali(input):
+	lang_re = re.compile(r'^(-nepali)|(--n)$')
+	return bool(lang_re.match(input))
+
+def IsXML(input):
+	file_re = re.compile(r'^[a-z]+.xml$')
+	return bool(file_re.match(input))
+
+def ArgumentParser():
+	arguments = sys.argv[1::]
+	filename = None
+	group = -1
+	twelveHr = False
+	nepali = False
+	for arg in arguments:
+		try:
+			if IsGroup(arg):
+				group = int(arg[1])
+				print(group)
+			elif IsTime(arg):
+				twelveHr = True
+				print(arg)
+			elif IsNepali(arg):
+				nepali = True
+				print(arg)
+			elif IsXML(arg):
+				filename = arg
+				print(arg)
+			else:
+				raise ArgumentError("invalid arguments ... cannot load :D")
+		except ArgumentError as e:
+			e.display()
+			return
+	
+	try:
+		if not filename:
+			raise ArgumentError("Invalid filename ... cannot laod :D")
+		else:
+			RoutineReader.main(filename, group, nepali, twelveHr)
+	except ArgumentError as e:
+		e.display()
+
+
 
 def main():
-	RoutineReader.main("test.xml")
+	#RoutineReader.main("test.xml")
+	ArgumentParser()
 
 if __name__=="__main__":
 	main()
