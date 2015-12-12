3865bec1f2501c65a8fd6e4b44d24bb5a2b1a2b6
bibek <bibekdahal.bd16@gmail.com>
2015-12-12 12:23:18 +0545

    Fix mainbatti-indicator.desktop

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ text = '\n\n# Added by mainbatti-indicator\n'
 text += 'export PATH=\${PATH}:' + path + '\n'
 os.system('bash -c \'grep -q -F "' + path + '" ~/.bashrc || echo "' + text + '" >> ~/.bashrc\'')
 
-deskfile = path+"/mainbatti-indicator.desktop"
+deskfile = path+"/mainbatti-indicator-template.desktop"
+deskfile2 = path+"/mainbatti-indicator.desktop"
 
 with open(deskfile,'r') as f:
     newlines = []
@@ -18,7 +19,7 @@ with open(deskfile,'r') as f:
         newlines.append(line.replace('<path>', path).replace('<icon>', path+"/mainbatti.png"))
 
 
-with open(deskfile, 'w') as f:
+with open(deskfile2, 'w') as f:
     for line in newlines:
         f.write(line)
 
@@ -30,8 +31,8 @@ finalpath2 = os.path.expanduser('~') + "/.local/share/applications/"
 if not os.path.exists(finalpath2):
     os.makedirs(finalpath2)
 
-shutil.copyfile(deskfile, finalpath+"mainbatti-indicator.desktop")
-shutil.copyfile(deskfile, finalpath2+"mainbatti-indicator.desktop")
+shutil.copyfile(deskfile2, finalpath+"mainbatti-indicator.desktop")
+shutil.copyfile(deskfile2, finalpath2+"mainbatti-indicator.desktop")
 
 if not os.fork():
     os.system('python3 "' + path+"/mainbatti-indicator.py" + '"')
